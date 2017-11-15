0aed3202c05f85e15bcc4f67ace6bf59c5e66767
bestie <alexfilth186@gmail.com>
2017-11-15 19:59:32 +0200

    Draft structure added

---
+++ android/src/org/jbestie/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.jbestie.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.jbestie.game.Match3;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Match3(), config);
+	}
+}
---
+++ desktop/src/org/jbestie/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.jbestie.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.jbestie.game.Match3;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Match3(), config);
+	}
+}
