c27b03b285c6ff8353684ca2ad89d563d875c325
bestie <alexfilth186@gmail.com>
2017-11-20 19:28:57 +0200

    Added GameStage to use instead of Match3 in future and will add menu-logic to MenuStage

---
+++ core/src/org/jbestie/game/stage/GameStage.java
@@ -0,0 +1,7 @@
+package org.jbestie.game.stage;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class GameStage extends Stage {
+
+}
---
+++ core/src/org/jbestie/game/stage/MenuStage.java
@@ -0,0 +1,7 @@
+package org.jbestie.game.stage;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class MenuStage extends Stage {
+
+}
