f724bc808952904f061294b999a35ef551311832
bestie <alexfilth186@gmail.com>
2017-11-23 21:33:01 +0200

    Added stage to GameScreen. Introduced some bugs with input handling because of multiple input. Please fix them and rewrite this @#$#!

--- core/src/org/jbestie/game/stage/GameScreen.java
+++ core/src/org/jbestie/game/stage/GameScreen.java
@@ -1,16 +1,19 @@
 package org.jbestie.game.stage;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.GlyphLayout;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import org.jbestie.game.Match3;
@@ -32,6 +35,7 @@ public class GameScreen implements Screen {
     private static final float ANIMATION_DURATION = 0.2f;
     private SpriteBatch batch;
     private Sprite background;
+    private Sprite menuBackground;
     private List<Texture> textures;
     private GameObject[][] gameMap;
     private List<GameObject> selectedElements = new ArrayList<GameObject>();
@@ -43,22 +47,32 @@ public class GameScreen implements Screen {
     private int score = 0;
 
     private GameState gameState = GameState.IDLE;
-    private Match3 game;
+    private final Match3 game;
     private List<Animation> animatedObjects = new ArrayList<Animation>();
     private Camera camera;
+    private Stage stage;
 
-
-    GameScreen(Match3 game) {
+    GameScreen(final Match3 game) {
         camera = new OrthographicCamera(Gdx.graphics.getWidth(),
                 Gdx.graphics.getHeight());
         camera.position.set(GameConstants.WORLD_WIDTH / 2, GameConstants.WORLD_HEIGHT / 2, 0);
         camera.update();
         viewport = new FitViewport(GameConstants.WORLD_WIDTH, GameConstants.WORLD_HEIGHT, camera);
+        stage = new Stage(viewport, game.batch);
+
+        stage.addListener(new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                return false;
+            }
+        });
 
         batch = game.batch;
         this.game = game;
         background  = new Sprite( new Texture(Gdx.files.internal("background/bg.png")));
         background.setPosition(0.0f, 0.0f);
+        menuBackground  = new Sprite( new Texture(Gdx.files.internal("background/blocking_panel_bg.png")));
+        menuBackground.setPosition(0.0f, 0.0f);
         font  = new BitmapFont();
 
         Texture blueJelly = new Texture(Gdx.files.local("sprites/jelly_blue.png"));
@@ -72,7 +86,65 @@ public class GameScreen implements Screen {
         gameMap = generateField();
         checkMatchesAndFillEmptyCells();
 
-        Gdx.input.setInputProcessor(new MatchThreeInputProcessor(this, selectedElements));
+        InputMultiplexer inputMultiplexer = new InputMultiplexer();
+        inputMultiplexer.addProcessor(stage);
+        inputMultiplexer.addProcessor(new MatchThreeInputProcessor(this, selectedElements));
+
+//        Gdx.input.setInputProcessor(new MatchThreeInputProcessor(this, selectedElements));
+
+        final Button restart = createRestartButton(game);
+        final Button levelSelect = createLevelRestartButton(game);
+        stage.addActor(restart);
+        stage.addActor(levelSelect);
+        Gdx.input.setInputProcessor(inputMultiplexer);
+    }
+
+    private Button createLevelRestartButton(final Match3 game) {
+        final Button levelSelect = new Button(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.local("sprites/choose_level.png")))));
+
+        float buttonWidth = GameConstants.WINDOW_WIDTH / 8;
+        float buttonHeight = GameConstants.WINDOW_WIDTH / 8;
+        levelSelect.setPosition(GameConstants.WINDOW_WIDTH / 2 - buttonWidth, GameConstants.WINDOW_HEIGHT / 2 - buttonWidth );
+        levelSelect.setSize(buttonWidth, buttonHeight);
+        levelSelect.addListener(new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                if (gameState != GameState.PAUSED) {
+                    return false;
+                }
+
+                if (levelSelect.isPressed()) {
+                    game.setScreen(new LevelSelectScreen(game));
+                    return true;
+                }
+                return false;
+            }
+        });
+        return levelSelect;
+    }
+
+    private Button createRestartButton(final Match3 game) {
+        final Button restart = new Button(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.local("sprites/restart_button.png")))));
+
+        float buttonWidth = GameConstants.WINDOW_WIDTH / 8;
+        float buttonHeight = GameConstants.WINDOW_WIDTH / 8;
+        restart.setPosition(GameConstants.WINDOW_WIDTH / 2 + buttonWidth / 2, GameConstants.WINDOW_HEIGHT / 2 - buttonWidth );
+        restart.setSize(buttonWidth, buttonHeight);
+        restart.addListener(new EventListener() {
+            @Override
+            public boolean handle(Event event) {
+                if (gameState != GameState.PAUSED) {
+                    return false;
+                }
+
+                if (restart.isPressed()) {
+                    game.setScreen(new GameScreen(game));
+                    return true;
+                }
+                return false;
+            }
+        });
+        return restart;
     }
 
 
@@ -85,21 +157,24 @@ public class GameScreen implements Screen {
     public void render (float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
 
         //place draw logic here
         switch (gameState) {
             case IDLE:
+                batch.begin();
                 drawBackground();
                 animateMatchedElementsBeforeRemoving();
                 processSelectedElements();
                 drawField(gameMap, batch);
                 drawScore();
+                batch.end();
                 break;
             case PAUSED:
-                drawMenu();
+                drawMenuBackground();
+                stage.draw();
                 break;
             case MOVING_ITEMS:
+                batch.begin();
                 if (animationsFinished()) {
                     gameState = GameState.IDLE;
                     animatedObjects.clear();
@@ -108,8 +183,10 @@ public class GameScreen implements Screen {
                 animate();
                 drawField(gameMap, batch);
                 drawScore();
+                batch.end();
                 break;
             case REMOVING_MATCHES:
+                batch.begin();
                 if (animationsFinished()) {
                     checkMatchesAndFillEmptyCells();
                     gameState = GameState.IDLE;
@@ -119,6 +196,7 @@ public class GameScreen implements Screen {
                 animate();
                 drawField(gameMap, batch);
                 drawScore();
+                batch.end();
                 break;
             case GAME_OVER:
                 break;
@@ -128,14 +206,15 @@ public class GameScreen implements Screen {
                 throw new IllegalStateException("State " + gameState + " is unknown!");
         }
 
-        //end of draw logic
-
-        batch.end();
-    }
-
-    private void drawMenu() {
 
     }
+
+    private void drawMenuBackground() {
+        stage.getBatch().begin();
+        stage.getBatch().draw(menuBackground, 0.0f, 0.0f, GameConstants.WINDOW_WIDTH, GameConstants.WINDOW_HEIGHT);
+        stage.getBatch().end();
+    }
+
 
     @Override
     public void dispose () {
--- core/src/org/jbestie/game/stage/LevelSelectScreen.java
+++ core/src/org/jbestie/game/stage/LevelSelectScreen.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Event;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
@@ -22,7 +21,6 @@ public class LevelSelectScreen implements Screen {
     private final Stage stage;
     private Texture background;
 
-
     LevelSelectScreen(final Match3 game) {
         background = new Texture(Gdx.files.local("sprites/level_select.png"));
 
@@ -31,14 +29,14 @@ public class LevelSelectScreen implements Screen {
         camera.position.set(GameConstants.WORLD_WIDTH / 2, GameConstants.WORLD_HEIGHT / 2, 0);
         camera.update();
 
-        stage = new Stage( new FitViewport(GameConstants.WORLD_WIDTH, GameConstants.WORLD_HEIGHT, camera)); //Set up a stage for the ui
+        stage = new Stage( new FitViewport(GameConstants.WORLD_WIDTH, GameConstants.WORLD_HEIGHT, camera), game.batch); //Set up a stage for the ui
         Gdx.input.setInputProcessor(stage);
 
 
         for (int i = 0; i < LEVELS_COUNT; i++) {
             float buttonWidth = GameConstants.WORLD_WIDTH / 8;
 
-            Button btn = new Button(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.local("sprites/level_"+ (i + 1)+ ".png")))));
+            final Button btn = new Button(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.local("sprites/level_"+ (i + 1)+ ".png")))));
             float buttonHeight = 1.5f * buttonWidth;
             int xValue = i % 4;
             int yValue = i / 4;
@@ -47,10 +45,9 @@ public class LevelSelectScreen implements Screen {
             btn.addListener(new EventListener() {
                 @Override
                 public boolean handle(Event event) {
-                    if (event instanceof InputEvent) { // dumb part of dumb code
-                        if (((InputEvent)event).getType() == InputEvent.Type.touchDown) {
-                            game.setScreen(new GameScreen(game));
-                        }
+                    if (btn.isPressed()) {
+                        game.setScreen(new GameScreen(game));
+                        return true;
                     }
                     return false;
                 }
